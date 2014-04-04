9b56a2bb39f7070ee069a28616fb3035503c260c
nperri6 <nperri6@gmail.com>
2014-04-03 20:18:51 -0700

    week 3 excercise 11

---
+++ wk3_ex11.rb
@@ -0,0 +1,14 @@
+#wk3_ex11
+#given an array prints each number and says odd or even
+
+def odd_or_even(array)
+  odd_or_even = []
+  array.each do |num|
+    num % 2 == 0? odd_or_even << "even": odd_or_even << "odd"
+  end
+  array.each_with_index do |num, index|
+    p "#{num} is an #{odd_or_even[index]} number."
+  end
+end
+
+odd_or_even([12, 23, 456, 123, 4579])
